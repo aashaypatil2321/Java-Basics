4a46915030d6c45d043ae1101524be26e8a8e347
aashaypatil2321 <aashaypatil2321@gmail.com>
2025-09-02 10:44:11 +0530

    Add files via upload

---
+++ WeekDays.java
@@ -0,0 +1,34 @@
+public class WeekDays {
+    //we have using the switch 
+    public static void main(String[] args) {
+        int n=8;
+        switch(n){
+            case 1:
+            System.out.println("Monday");
+            break;
+              case 2:
+            System.out.println("tuesday");
+            break;  
+            case 3:
+            System.out.println("wednesday");
+            break;  
+            case 4:
+            System.out.println("thursday");
+            break;  
+            case 5:
+            System.out.println("friday");
+            break; 
+             case 6:
+            System.out.println("saturday");
+            break; 
+             case 7:
+            System.out.println("sunday");
+            break;
+            default:
+            System.out.println("Enter a valid Number");
+
+        }
+
+    }
+    
+}
